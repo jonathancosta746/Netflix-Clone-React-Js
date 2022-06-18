241d1549b0fd74a623610f41693af321ea513ced
Jonathan Costa <jonathancosta746@gmail.com>
2022-06-18 00:33:58 -0300

    adicionado css listas de filmes

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -3,12 +3,16 @@ import "./MovieRow.css";
 
 export default ({title, items})=>{
     return (
-        <div>
+        <div className="movieRow">
             {title}
             <div className="movieRow--listarea">
-                {items.results.length > 0 && items.results.map((item, key)=>(
-                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
-                ))}    
+                <div className="movieRow--list">
+                    {items.results.length > 0 && items.results.map((item, key)=>(
+                        <div key={key} className="movieRow--item">
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
+                        </div>
+                    ))}  
+                </div>              
             </div>
         </div>
     );
