f8e781e32be49a12febd4ffbcc8a7431f51ab659
Jonathan Costa <jonathancosta746@gmail.com>
2022-06-20 09:33:13 -0300

    adicionado favicon

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -5,7 +5,7 @@ import ArowLeft from './images/material-left.png';
 
 
 export default ({title, items})=>{
-    const [scrollX, setScrollX] = useState(-400)
+    const [scrollX, setScrollX] = useState(0)
     const handleLeftArrow = () => {
         let x = scrollX + Math.round(window.innerWidth / 2);
         if(x > 0) {
