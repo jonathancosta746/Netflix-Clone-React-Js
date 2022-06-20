1906fc5d7e931798dac28fbc410734686adf98e9
Jonathan Costa <jonathancosta746@gmail.com>
2022-06-19 23:37:54 -0300

    finalizada função para avançar filmes na lista

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,25 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import './Style.css';
 import ArowRight from './images/material-right.png';
 import ArowLeft from './images/material-left.png';
 
 
 export default ({title, items})=>{
+    const [scrollX, setScrollX] = useState(-400)
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0) {
+            x=0;
+        }
+        setScrollX(x);
+    }
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW = items.results.length * 150;
+        if((window.innerWidth - listW) > x) {
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollX(x);
+    }
+
     return (
         <div className="movieRow">
             <div className="title--row">{title}</div>
             
 
-            <div className="movieRow--left">
+            <div className="movieRow--left" onClick={handleLeftArrow}>
                 <img src={ArowLeft}></img>
             </div>
-            <div className="movieRow--right">
+            <div className="movieRow--right" onClick={handleRightArrow}>
                 <img src={ArowRight}></img>
             </div>
             
             
             <div className="movieRow--listarea">
-                <div className="movieRow--list">
+                <div className="movieRow--list" style={{
+                    marginLeft: scrollX,
+                    width: items.results.length * 150
+                }}>
                     {items.results.length > 0 && items.results.map((item, key)=>(
                         <div key={key} className="movieRow--item">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
